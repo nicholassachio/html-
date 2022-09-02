9e4df08a95f43e8cc10c0dd6bf3a4304ae6cbf65
Nicholas sachio <111225162+nicholassachio@users.noreply.github.com>
2022-09-02 08:35:27 +0700

    Add files via upload

---
+++ hellocpp.cpp
@@ -0,0 +1,18 @@
+include#ide<iostream>
+using namespace std ;
+int main (){
+cout<<"hello world"<<endl;
+cout<<"nama :nicholas sachio"<<endl;
+cout<<"kelas :xpplg1"<<endl;
+cout<<"=========="<<endl;
+cout<<"angka1=10"<<endl;
+cout<<"angka2=5"<<endl;
+cout<<"hasil;" <<10+5<<endl;
+cout<<"hasil;"<<10-5<<endl;
+cout<<"hasil;"<<10*5<<endl;
+cout<<"hasil;"<<10/5<<endl;
+cout<<"hasil;"<<50-10+30*2/7<<endl;
+return 0;
+
+}
+-
